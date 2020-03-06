d15f9d0bb63c7f34cdc3408337800c7cdbc0145e
Shinpei Maruyama <shinpeim@gmail.com>
2020-03-06 23:57:32 +0900

    Initial commit

---
+++ build.rb
@@ -0,0 +1,22 @@
+require 'kramdown'
+
+markdown = File.read('index.md')
+body = Kramdown::Document.new(markdown).to_html
+
+html =<<-EOD
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="./css/reset.css" />
+  <link rel="stylesheet" href="./css/style.css" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+</head>
+
+<body>
+#{body}
+</body>
+</html>
+EOD
+File.write('docs/index.html', html)
